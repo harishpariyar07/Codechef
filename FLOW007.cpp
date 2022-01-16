63f8bd7850bd10d03016b004d3b7c6049bb97596
harishpariyar07 <69762926+harishpariyar07@users.noreply.github.com>
2022-01-16 17:48:51 +0530

    Program solved using vector instead of arrays

--- FLOW007.cpp
+++ FLOW007.cpp
@@ -3,31 +3,26 @@
 // Problem link: https://www.codechef.com/LRNDSA01/problems/FLOW007
 // Solution link: https://www.codechef.com/viewsolution/49097539
 #include <iostream>
+#include <vector>
 using namespace std;
 
-int main()
-{
-    // your code goes here
-    int n;
-    cin >> n;
-    int nums[n];
-
-    for (int i = 0; i < n; i++)
-    {
-        cin >> nums[i];
-    }
-
-    for (int i = 0; i < n; i++)
-    {
-        int rev = 0;
-        int num = nums[i];
-        while (num > 0)
-        {
-            int x = num % 10;
-            rev = rev * 10 + x;
-            num /= 10;
-        }
-        cout << rev << endl;
-    }
-    return 0;
+int main() {
+	// your code goes here
+	int t;
+	cin >> t;
+	vector<int> nums(t);
+	for (int i = 0; i < t; i++)
+	{
+	    cin >> nums[i];
+	    int n = nums[i], rev = 0;
+	    while (n > 0)
+	    {
+	       int r = n % 10;
+	       rev = rev * 10 + r;
+	       n /= 10;
+	    }
+	    cout << rev << endl;
+	}
+	return 0;
 }
+
