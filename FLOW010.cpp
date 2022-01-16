3fda961aa6e5aaa220a059046f29d23af1bd3e6a
Harish Pariyar <harishpariyar@Harishs-MacBook-Air.local>
2022-01-16 17:54:39 +0545

    code added

---
+++ FLOW010.cpp
@@ -0,0 +1,39 @@
+// Write a program that takes in a letterclass ID of a ship and display the equivalent string class description of the given ID.
+// Problem link: https://www.codechef.com/problems/FLOW010
+// Solution link: https://www.codechef.com/viewsolution/56486368
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    // your code goes here
+    // no of testcases
+    int n;
+    cin >> n;
+    char c[n];
+    for (int i = 0; i < n; i++)
+    {
+        cin >> c[i];
+        if (c[i] == 'B' || c[i] == 'b')
+        {
+            cout << "BattleShip" << endl;
+        }
+        else if (c[i] == 'C' || c[i] == 'c')
+        {
+            cout << "Cruiser" << endl;
+        }
+        else if (c[i] == 'D' || c[i] == 'd')
+        {
+            cout << "Destroyer" << endl;
+        }
+        else if (c[i] == 'F' || c[i] == 'f')
+        {
+            cout << "Frigate" << endl;
+        }
+        else
+        {
+            continue;
+        }
+    }
+    return 0;
+}
